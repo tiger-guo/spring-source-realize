a53da114fedd1dd8bc29fd8a63fc2d4b0922536a
liuguohu <1774687347@qq.com>
2020-03-17 14:20:04 +0800

    初始化仓库

---
+++ src/test/java/com/spring/realize/SpirngRealizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.realize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngRealizeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
